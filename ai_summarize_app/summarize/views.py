4aecdc07c201ea0fd64e02dcd2d48574b14099ad
ashsajal1 <ashsajal1@gmail.com>
2024-02-10 19:07:21 +0800

    ai model works

--- ai_summarize_app/summarize/views.py
+++ ai_summarize_app/summarize/views.py
@@ -1,6 +1,14 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+# Use a pipeline as a high-level helper
+from transformers import pipeline
+
+pipe = pipeline("summarization", model="Falconsai/text_summarization")
 
 # Create your views here.
 def summarize(request):
-    return HttpResponse("Hello world!")
+  summarized_text = pipe("This si a sample text")[0]['summary_text']
+  return HttpResponse(summarized_text)
+
+
+    
